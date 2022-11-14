66ba818972632c2dcd83d3e4d2436d0e38daaf70
Romain Bioteau <romain.bioteau@bonitasoft.com>
2022-11-14 12:44:56 +0100

    feat(bdm): bdm sources generation
    Add `generate-bdm-model` and `generate-bdm-dao-client` goals.
    Those goals can be used to generated java sources from a Business Object
    Model descriptor.
    
    Closes [DEV-6](https://bonitasoft.atlassian.net/browse/DEV-6)

---
+++ plugin/src/main/java/org/bonitasoft/plugin/bdm/codegen/GenerateBdmDaoClientSourceMojo.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2022 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.plugin.bdm.codegen;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+import javax.inject.Inject;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.sonatype.plexus.build.incremental.BuildContext;
+
+/**
+ * This goal generates the BDM dao client java source code from the given Business Object Model descriptor file.
+ *
+ */
+@Mojo(name = "generate-bdm-dao-client", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true)
+public class GenerateBdmDaoClientSourceMojo extends AbstractGenerateBdmMojo {
+
+    @Inject
+    public GenerateBdmDaoClientSourceMojo(BusinessDataModelParser businessDataModelReader,
+            BusinessDataModelGenerator generator, BuildContext buildContext) {
+        super(businessDataModelReader, generator, buildContext);
+    }
+    
+    @Override
+    protected Predicate<Path> exludedGeneratedSources() {
+        return file -> {
+            String fileName = file.getFileName().toString();
+            return fileName.endsWith(".java") && !fileName.endsWith("DAOImpl.java");
+        };
+    }
+
+
+}
---
+++ plugin/src/main/java/org/bonitasoft/plugin/bdm/codegen/GenerateBdmModelSourceMojo.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2022 BonitaSoft S.A.
+ * BonitaSoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bonitasoft.plugin.bdm.codegen;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+import javax.inject.Inject;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.sonatype.plexus.build.incremental.BuildContext;
+
+/**
+ * This goal generates the BDM model java source code from the given Business Object Model descriptor file.
+ *
+ */
+@Mojo(name = "generate-bdm-model", defaultPhase = LifecyclePhase.GENERATE_SOURCES, threadSafe = true)
+public class GenerateBdmModelSourceMojo extends AbstractGenerateBdmMojo {
+
+    @Inject
+    public GenerateBdmModelSourceMojo(BusinessDataModelParser businessDataModelReader,
+            BusinessDataModelGenerator generator, BuildContext buildContext) {
+        super(businessDataModelReader, generator, buildContext);
+    }
+
+    /**
+     * Only keep source files relevant for the bdm-model module.
+     */
+    @Override
+    protected Predicate<Path> exludedGeneratedSources() {
+        return file -> file.getFileName().toString().endsWith("DAOImpl.java");
+    }
+
+}
