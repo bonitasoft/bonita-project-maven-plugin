0a10ffe3ff6f3d9cd4d76084336a439d3cde492b
Adrien Kantcheff <adrien.kantcheff@bonitasoft.com>
2023-08-30 15:28:02 +0200

    chore(test): use existing invoker variable for local repo

--- plugin/src/it/project-with-dep-in-store/verify.groovy
+++ plugin/src/it/project-with-dep-in-store/verify.groovy
@@ -1,12 +1,12 @@
 import java.io.*;
 
 // Dependency from the project store should be installed in the local repository
-File jarFile = new File( localRepo, 'org/bonitasoft/in-store/0.0.1-SNAPSHOT/in-store-0.0.1-SNAPSHOT.jar');
+File jarFile = new File( localRepositoryPath, 'org/bonitasoft/in-store/0.0.1-SNAPSHOT/in-store-0.0.1-SNAPSHOT.jar');
 if ( !jarFile.isFile() ) {
     throw new FileNotFoundException( "Could not find installed JAR: " + jarFile );
 }
 
-File pomFile = new File( localRepo, 'org/bonitasoft/in-store/0.0.1-SNAPSHOT/in-store-0.0.1-SNAPSHOT.pom');
+File pomFile = new File( localRepositoryPath, 'org/bonitasoft/in-store/0.0.1-SNAPSHOT/in-store-0.0.1-SNAPSHOT.pom');
 if ( !pomFile.isFile() ) {
     throw new FileNotFoundException( "Could not find installed pom: " + pomFile );
 }
