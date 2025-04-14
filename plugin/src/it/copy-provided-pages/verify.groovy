3bfcc7a668e723a6fe6544da8ba22c27897ff088
Vincent Hémery <vincent.hemery@bonitasoft.com>
2025-04-14 10:21:51 +0200

    Update Bonita runtime version
    Application model must be compatible with bonita-process-model 9.0.2.
    Hence, only Bonita 10.2+ is supported (with application links).

--- plugin/src/it/copy-provided-pages/verify.groovy
+++ plugin/src/it/copy-provided-pages/verify.groovy
@@ -8,11 +8,13 @@ outputFolder.eachFileRecurse(FileType.FILES) {
     files << it.getName()
 }
 
+def bonitaVersion = '10.2.0'
+
 def expectedFiles = [
-        'page-user-case-details-8.0.0.zip',
-        'page-user-case-list-8.0.0.zip',
-        'page-user-process-list-8.0.0.zip',
-        'page-user-task-list-8.0.0.zip'
+        'page-user-case-details-' + bonitaVersion + '.zip',
+        'page-user-case-list-' + bonitaVersion + '.zip',
+        'page-user-process-list-' + bonitaVersion + '.zip',
+        'page-user-task-list-' + bonitaVersion + '.zip'
 ]
 assert expectedFiles.size() == files.size(): "Expected ${expectedFiles.size()} files but was ${files.size()}"
 assert expectedFiles.containsAll(files): "Some expected files are missing"
